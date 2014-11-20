f660255a64087949ef2ef9f3b80ff578b838f5ad
Administrator <Administrator@qian_sheng.HOLD.FOUNDER.COM>
2014-11-20 10:43:01 +0800

    add jmx

--- src/main/java/schedule/client/SpringClient.java
+++ src/main/java/schedule/client/SpringClient.java
@@ -1,11 +1,17 @@
 package schedule.client;
 
+import java.io.IOException;
+
+import mx4j.tools.adaptor.http.HttpAdaptor;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class SpringClient {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		  ApplicationContext context = new ClassPathXmlApplicationContext("schedule/spring-schedule.xml");
+		  HttpAdaptor httpAdaptor = (HttpAdaptor) context.getBean("httpAdaptor");  
+	      httpAdaptor.start();  
 	}
 }
--- src/main/java/schedule/service/DailyEmailJob.java
+++ src/main/java/schedule/service/DailyEmailJob.java
@@ -1,5 +1,8 @@
 package schedule.service;
 
+
+import jmx.service.Hello;
+
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
@@ -9,6 +12,12 @@ public class DailyEmailJob extends QuartzJobBean {
 	public void setScheduleService(ScheduleService scheduleService) {
 		this.scheduleService = scheduleService;
 	}
+	
+	private Hello hello;
+	public void setHello(Hello hello) {
+		this.hello = hello;
+	}
+	
 	public DailyEmailJob() {
 		// TODO Auto-generated constructor stub
 	}
@@ -18,6 +27,7 @@ public class DailyEmailJob extends QuartzJobBean {
 			throws JobExecutionException {
 		// TODO Auto-generated method stub
 		scheduleService.sendDailyEmails();
+		hello.getCacheSize();
 	}
 	
 	
